670efd64abf17d241eb14c269af546135b81d476
JZacharie <votre-email@exemple.com>
2026-06-25 08:57:04 +0200

    fix: cargo fmt sort imports

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use tracing_subscriber::EnvFilter;
 
 #[cfg(windows)]
 use windows_sys::Win32::Security::Cryptography::{
-    CertAddEncodedCertificateToStore, CertCloseStore, CertDeleteCertificateFromStore,
-    CertOpenStore, CERT_CONTEXT, CERT_STORE_ADD_REPLACE_EXISTING, CERT_STORE_PROV_SYSTEM_W,
-    CERT_SYSTEM_STORE_CURRENT_USER, HCERTSTORE, PKCS_7_ASN_ENCODING, X509_ASN_ENCODING,
+    CERT_CONTEXT, CERT_STORE_ADD_REPLACE_EXISTING, CERT_STORE_PROV_SYSTEM_W,
+    CERT_SYSTEM_STORE_CURRENT_USER, CertAddEncodedCertificateToStore, CertCloseStore,
+    CertDeleteCertificateFromStore, CertOpenStore, HCERTSTORE, PKCS_7_ASN_ENCODING,
+    X509_ASN_ENCODING,
 };
 
 fn init_logger(
